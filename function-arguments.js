863807fcbd15a87e674935c88e9758e24f72bdd5
Rabhas <rabhas.vashisht@gmail.com>
2022-02-19 00:18:11 +0000

    Continue with OBJECTS, OBJECT PROPERTIES, and FUNCTIONS

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (x,y,z){
+  return x = y*z+x
+}
+console.log(math(53,61,67)); 
---
+++ object-keys.js
@@ -0,0 +1,7 @@
+let myCar = {
+  make: 'hyundai',
+  model: 'santafe',
+  year: 2020
+}
+const keys = Object.keys(myCar)
+console.log(keys)
